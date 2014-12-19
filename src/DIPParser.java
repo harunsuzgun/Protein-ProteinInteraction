b130e857b5c8ac6d623e5fbe88b104fda763366e
Selman <aliselmanaydin@gmail.com>
2014-12-19 20:26:28 +0200

    First version

---
+++ src/DIPParser.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Aydin
+ * Date: 12/19/14
+ * Time: 8:20 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DIPParser {
+
+    private String sequencesPath;
+    private String interactionsPath;
+
+    public DIPParser(String sequencesPath,String interactionsPath){
+
+        this.sequencesPath = sequencesPath;
+        this.interactionsPath = interactionsPath;
+
+    }
+
+    public ArrayList<Protein> RetrieveProteins()
+    {
+
+
+    }
+
+
+
+
+}
--- src/ReadXMLFile.java
+++ src/ReadXMLFile.java
@@ -4,43 +4,24 @@ import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
 import org.w3c.dom.Element;
-import java.io.File;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class ReadXMLFile {
 
     public static void main(String argv[]) {
-        ArrayList IDs = new ArrayList();
-        try {
 
-            File fXmlFile = new File("C:\\Users\\Celebi\\Desktop\\Downloads\\allppis.xml");
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(fXmlFile);
+        String xmlUrl = "C:\\Users\\Celebi\\Desktop\\Downloads\\allppis.xml";
+        ArrayList<String> proteinIDs = XmlProcessor.RetrieveProteinIDs(xmlUrl);
 
-            doc.getDocumentElement().normalize();
+        ProteinParser parser = new ProteinParser();
 
-            System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
-            NodeList nList = doc.getElementsByTagName("primaryRef");  // hacı bu bizim id'nin referansı oluyor
-            System.out.println("----------------------------");
-
-            for (int temp = 0; temp < nList.getLength(); temp++) {
-
-                Node nNode = nList.item(temp);
-                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                    Element eElement = (Element) nNode;
-                    //System.out.println(eElement.getAttribute("db"));
-                    if(eElement.getAttribute("db").equals("SP"))
-                        IDs.add(eElement.getAttribute("id"));
-                    //System.out.println("Protein id : " + eElement.getAttribute("id"));
-
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        //System.out.println(IDs);
+        ArrayList<Protein> proteins = new ArrayList<Protein>();
+        for(String id:proteinIDs)
+            proteins.add(parser.getProtein(id));
 
 
 
