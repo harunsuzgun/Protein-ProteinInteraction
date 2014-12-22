3a0d51f2d6244d6dd83c1a2bdc0dbafadad069b8
Selman <aliselmanaydin@gmail.com>
2014-12-22 12:25:30 +0200

    Added jars and import statements.

--- src/ReadXMLFile.java
+++ src/ReadXMLFile.java
@@ -1,21 +1,17 @@
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.DocumentBuilder;
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Node;
-import org.w3c.dom.Element;
-
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 
+import org.graphstream.graph.implementations.DefaultGraph;
+import weka.*;
+import org.graphstream.graph.*;
 public class ReadXMLFile {
 
     public static void main(String argv[]) {
 
         DIPParser parser = new DIPParser("D:\\fastaseq\\fasta20131201.seq","D:\\fasta20131201.seq\\dip20141001.txt");
         ArrayList<DIPProtein> proteins = parser.RetrieveProteins();
+
+        Graph graph = new DefaultGraph("PIP Network");
+
 
 
     }
